e7237a9cb00a4822853789cb59a6e89714e863c4
Balint Bakos <balint.bb@gmail.com>
2020-06-18 11:46:43 +0200

    adds some more functions to advanced

--- Advanced/src/app.ts
+++ Advanced/src/app.ts
@@ -31,15 +31,34 @@ class Car extends Vehicles {
     
     constructor(public type: string, model: string, hp: number, price: number, seats: number, color: string) {
         super(model, hp, price, seats, color)
+
+        this.generateButton()
     }
 
     getInfo() {
         return `Type: ${this.type}
                 ${super.getInfo()}`
     }
+
+    private generateButton(): void {
+        const divEl = document.getElementById('results')! as HTMLDivElement
+        const instanceDivEl = document.createElement('div')
+        const buttonEl = document.createElement('button')
+        buttonEl.addEventListener('click', () => {
+            const pEl = document.createElement('p')
+            pEl.innerText = `${this.hp} ${this.type}`
+            divEl.append(pEl)
+
+        })
+        buttonEl.innerText = this.model
+        instanceDivEl.append(buttonEl)
+        divEl.append(instanceDivEl)
+    }
+
+    
 }
 
-// let myCar = new Car ("Electro-Car", "Tesla", 600, 70000, 5, "red");
+let myCar = new Car ("Electro-Car", "Tesla", 600, 70000, 5, "red");
 
 class Truck extends Car {
     
@@ -57,3 +76,4 @@ let myTruck = new Truck ("Cooler", "MAN", 1200, 100000, 3, "white", 10.0);
 console.log(myTruck.getInfo());
 
 
+const truck2 = new Truck ("Cooler", "Volvo", 1200, 100000, 3, "white", 10.0);
